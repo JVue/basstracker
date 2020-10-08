e06fed5f82611f6a5d98b2d5373e05c6437c602a
Jason Vue <Jason.Vue@target.com>
2020-10-07 19:13:59 -0500

    added additional values that includes user input string

--- helpers/calculator.rb
+++ helpers/calculator.rb
@@ -1,21 +1,35 @@
 class Calculator
   def initialize(weights_str)
-    @weights = weights_str
-    @weights = sanitize_input_string
+    @weights_str = weights_str
   end
 
-  def sanitize_input_string
-    input_str = @weights.downcase
-    raise 'Input string contains a letter character.' if input_str.match(/[a-z]/)
-    convert_to_oz_array(input_str)
+  def create_arrays
+    sanitize_input_string(@weights_str)
+    @weights_input_array = convert_string_to_array(@weights_str)
+    @weights_input_array_in_oz = convert_to_oz_array(@weights_input_array)
   end
 
-  def convert_to_oz_array(weights_str)
+  def sanitize_input_string(weights_str)
+    raise 'Input string contains a letter character.' if weights_str.match(/[a-z]{1,}/)
+    weights_str.downcase
+  end
+
+  def convert_string_to_array(weights_str)
+    remove_spaces_from_array(weights_str.split(','))
+  end
+
+  def remove_spaces_from_array(array)
+    new_array = []
+    array.each do |element|
+      raise "Value: #{weight} is invalid." if (element.include?('-') && element.include?('.'))
+      new_array << element.gsub(' ', '')
+    end
+    new_array
+  end
+
+  def convert_to_oz_array(array)
     arr = []
-    weights_arr = convert_string_to_array(weights_str)
-    weights_arr.each do |weight|
-      raise 'A value is empty or contains too many spaces / invalid characters.' unless weight.match(/\d/)
-      raise "Value: #{weight} is invalid." if weight.include?('-') && weight.include?('.')
+    array.each do |weight|
       if weight.include?('-')
         arr << convert_lbs_oz_to_oz(weight)
       elsif weight.include?('.')
@@ -27,18 +41,20 @@ class Calculator
     arr
   end
 
-  def convert_string_to_array(weights_str)
-    return weights_str.split(',') if weights_str.include?(',')
-    weights_str.split(' ')
-  rescue StandardError => err
-    err.message
+  def convert_array_to_string(array)
+    array.to_s.gsub("\"", '').gsub(' ','').gsub('[', '').gsub(']', '').gsub(',', ' + ')
   end
 
   def total_weight
+    create_arrays
     {
-      'total_lbs_oz'  => lbs_oz_total(@weights.inject(:+)),
-      'total_decimal' => decimal_total(@weights.inject(:+))
+      'input_weights' => convert_array_to_string(@weights_input_array),
+      'input_weights_in_oz' => convert_array_to_string(@weights_input_array_in_oz),
+      'total_lbs_oz'  => lbs_oz_total(@weights_input_array_in_oz.inject(:+)),
+      'total_decimal' => decimal_total(@weights_input_array_in_oz.inject(:+))
     }
+  rescue StandardError => err
+    err.message
   end
 
   def lbs_oz_total(total_oz)
