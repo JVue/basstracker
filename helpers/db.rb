718b810664f7f91a280b4978b0c619fae915be33
Jason Vue <Jason.Vue@target.com>
2021-04-18 21:57:43 -0500

    added state field

--- helpers/db.rb
+++ helpers/db.rb
@@ -7,14 +7,14 @@ class DB
     @pg = PG.connect :hostaddr => Secrets.db_hostaddress, :dbname => Secrets.db_name, :user => Secrets.db_username, :password => Secrets.db_password
   end
 
-  def add_entry(date, time, angler, event, weight, weight_decimal, weight_oz, bass_type, state, lake = nil)
+  def add_weight_entry(date, time, angler, event, weight, weight_decimal, weight_oz, bass_type, state, lake = nil)
     response = @pg.exec "INSERT INTO basstracker (date, time, angler, event, weight, weight_decimal, weight_oz, bass_type, state, lake) VALUES(\'#{date}\', \'#{time}\', \'#{angler}\', \'#{event}\', \'#{weight}\', \'#{weight_decimal}\', \'#{weight_oz}\', \'#{bass_type}\', \'#{state}\', \'#{lake}\')"
     return false if response.result_status != 1
 
     true
   end
 
-  def get_entry(time, date, angler, event, weight, bass_type, state, lake)
+  def get_weight_entry(time, date, angler, event, weight, bass_type, state, lake)
     response = @pg.exec "SELECT * FROM basstracker WHERE date=\'#{date}\' AND time=\'#{time}\' AND angler=\'#{angler}\' AND event=\'#{event}\' AND weight=\'#{weight}\' AND bass_type=\'#{bass_type}\' AND state=\'#{state}\' AND lake=\'#{lake}\'"
     return false if response.result_status == 0
 
